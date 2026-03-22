b1fd7baf632e035bb065f9c284a31861feb86d29
Rahul <rahulsarma.k66@gmail.com>
2026-03-22 18:29:25 +0530

    LC:217

---
+++ arrays/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : nums) {
+            if (hs.contains(num))
+                return true;
+            hs.add(num);
+        }
+        return false;
+    }
+}
